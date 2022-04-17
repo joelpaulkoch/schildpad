a47e2dc3a9695d61bff9334fc4a134935aeb5d5a
Joel Koch <joel.koch@posteo.de>
2022-04-17 18:47:02 +0200

    add integration test support

---
+++ integration_test/app_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:schildpad/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('start app, it works', (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      expect(find.text('this is home'), findsOneWidget);
+    });
+  });
+}
