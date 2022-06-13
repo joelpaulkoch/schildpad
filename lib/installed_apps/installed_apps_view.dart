fc72ddcaa9557a53b68466afc2c3e19515823b15
Joel Koch <joel.koch@posteo.de>
2022-06-13 21:21:12 +0200

    show 3 columns of apps

--- lib/installed_apps/installed_apps_view.dart
+++ lib/installed_apps/installed_apps_view.dart
@@ -3,28 +3,40 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'installed_apps.dart';
 
+final _columnCountProvider = Provider<int>((ref) {
+  return 3;
+});
+
 class InstalledAppsView extends StatelessWidget {
   const InstalledAppsView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: CustomScrollView(slivers: <Widget>[
-      SliverAppBar(
-        pinned: false,
-        snap: false,
-        floating: false,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      const SliverPadding(
-          padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
-          sliver: InstalledAppsGrid()),
-    ]));
+    return Container(
+      decoration: const FlutterLogoDecoration(),
+      child: CustomScrollView(slivers: <Widget>[
+        SliverAppBar(
+          backgroundColor: Colors.transparent,
+          pinned: false,
+          snap: false,
+          floating: false,
+          actions: [
+            IconButton(
+              icon: const Icon(
+                Icons.settings_outlined,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+              splashColor: Colors.transparent,
+              splashRadius: 20,
+            ),
+          ],
+        ),
+        const SliverPadding(
+            padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+            sliver: InstalledAppsGrid())
+      ]),
+    );
   }
 }
 
@@ -36,10 +48,9 @@ class InstalledAppsGrid extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final installedApps = ref.watch(installedAppsProvider);
+    final columnCount = ref.watch(_columnCountProvider);
     return SliverGrid.count(
-        crossAxisCount: 4,
-        mainAxisSpacing: 16,
-        // crossAxisSpacing: 16,
+        crossAxisCount: columnCount,
         children: installedApps.maybeWhen(
             data: (appsList) => appsList
                 .map((app) => InstalledAppButton(
@@ -65,69 +76,28 @@ class InstalledAppButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Expanded(
-            child: IconButton(
-          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 0),
-          icon: icon,
-          onPressed: onTap,
-        )),
-        Text(
-          appName,
-          style: Theme.of(context).textTheme.caption,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          textAlign: TextAlign.center,
-        ),
-      ],
-    );
-  }
-}
-
-class InstalledAppsList extends ConsumerWidget {
-  const InstalledAppsList({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final installedApps = ref.watch(installedAppsProvider);
-    return SliverList(
-        delegate: SliverChildListDelegate(installedApps.maybeWhen(
-            data: (appsList) => appsList
-                .map((app) => InstalledAppListTile(
-                    appIcon: Image.memory(app.icon),
-                    appName: app.appName,
-                    onTap: app.openApp))
-                .toList(),
-            orElse: () => [])));
-  }
-}
-
-class InstalledAppListTile extends StatelessWidget {
-  const InstalledAppListTile(
-      {Key? key, required this.appIcon, required this.appName, this.onTap})
-      : super(key: key);
-
-  final Image appIcon;
-
-  final Function? onTap;
-
-  final String appName;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      shape: Border.symmetric(
-          horizontal: BorderSide(color: Theme.of(context).dividerColor)),
-      contentPadding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      leading: appIcon,
-      title: Text(
-        appName,
-        maxLines: 1,
+    return Material(
+      type: MaterialType.transparency,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          IconButton(
+            iconSize: 60,
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+            icon: icon,
+            onPressed: onTap,
+            splashColor: Colors.transparent,
+          ),
+          Text(
+            appName,
+            style: Theme.of(context).textTheme.caption,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
-      onTap: () => onTap,
-      horizontalTitleGap: 40,
     );
   }
 }
