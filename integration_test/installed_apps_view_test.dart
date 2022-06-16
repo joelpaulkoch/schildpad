e6d141d805bdf6b45a225e0fe3f98935f03ad8d5
Joel Koch <joel.koch@posteo.de>
2022-06-16 09:56:10 +0200

    test navigation to InstalledAppsView

--- integration_test/installed_apps_view_test.dart
+++ integration_test/installed_apps_view_test.dart
@@ -7,8 +7,8 @@ import 'package:schildpad/main.dart' as app;
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('basic InstalledAppsView tests', () {
-    testWidgets('Swiping up should open InstalledAppsView',
+  group('navigate to InstalledAppsView tests', () {
+    testWidgets('Swiping down should not open InstalledAppsView',
         (WidgetTester tester) async {
       app.main();
       await tester.pumpAndSettle();
@@ -19,13 +19,57 @@ void main() {
       expect(homeViewFinder, findsOneWidget);
 
       // When:
+      // I swipe down
+      await tester.fling(homeViewFinder, const Offset(0, 100), 500);
+      await tester.pumpAndSettle();
+
+      // Then:
+      // InstalledAppsView is not openend
+      expect(find.byType(InstalledAppsView), findsNothing);
+    });
+    testWidgets('Swiping up should open InstalledAppsView',
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+      // Given:
+      // I am on the HomeView
+      final homeViewFinder = find.byType(HomeView);
+      expect(homeViewFinder, findsOneWidget);
+
+      // When:
       // I swipe up
-      await tester.drag(homeViewFinder, const Offset(0, 500));
+      await tester.fling(homeViewFinder, const Offset(0, -100), 500);
       await tester.pumpAndSettle();
 
       // Then:
       // A list of all installed apps is shown
       expect(find.byType(InstalledAppsView), findsOneWidget);
+    });
+  });
+
+  group('add to HomeView tests', () {
+    testWidgets(
+        'Long pressing app icon should enable drag and drop to home view',
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      // Given:
+      // I am on the InstalledAppsView
+      final homeViewFinder = find.byType(HomeView);
+      expect(homeViewFinder, findsOneWidget);
+
+      await tester.fling(homeViewFinder, const Offset(0, -100), 500);
+      await tester.pumpAndSettle();
+
+      final installedAppsViewFinder = find.byType(InstalledAppsView);
+      // expect(installedAppsViewFinder, findsOneWidget);
+
+      // When:
+      // I swipe up
+
+      // Then:
+      // A list of all installed apps is shown
     });
   });
 }
