a517491d604bfc3c6a4ab7e5f4b1946970301c04
Joel Koch <joel.koch@posteo.de>
2022-07-23 15:16:44 +0200

    add and remove pages in page state

--- lib/home/pages.dart
+++ lib/home/pages.dart
@@ -1,12 +1,12 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final leftPagesProvider = StateProvider<int>((ref) {
-  return 0;
+final leftPagesProvider = Provider<int>((ref) {
+  return ref.watch(pagesProvider).leftPages;
 });
 
-final rightPagesProvider = StateProvider<int>((ref) {
-  return 0;
+final rightPagesProvider = Provider<int>((ref) {
+  return ref.watch(pagesProvider).rightPages;
 });
 
 final initialPageProvider = Provider<int>((ref) {
@@ -18,6 +18,47 @@ final pageCountProvider = Provider<int>((ref) {
   final right = ref.watch(rightPagesProvider);
   return left + 1 + right;
 });
+
+final pagesProvider =
+    StateNotifierProvider<PagesStateNotifier, PageCounter>((ref) {
+  return PagesStateNotifier();
+});
+
+class PagesStateNotifier extends StateNotifier<PageCounter> {
+  PagesStateNotifier() : super(const PageCounter(0, 0));
+
+  void addLeftPage() {
+    state = PageCounter(state.leftPages + 1, state.rightPages);
+  }
+
+  void addRightPage() {
+    state = PageCounter(state.leftPages, state.rightPages + 1);
+  }
+
+  void removeLeftPage() {
+    if (state.leftPages > 0) {
+      state = PageCounter(state.leftPages - 1, state.rightPages);
+    }
+  }
+
+  void removeRightPage() {
+    if (state.rightPages > 0) {
+      state = PageCounter(state.leftPages, state.rightPages - 1);
+    }
+  }
+}
+
+class PageCounter extends Equatable {
+  const PageCounter(this.leftPages, this.rightPages)
+      : assert(leftPages >= 0),
+        assert(rightPages >= 0);
+
+  final int leftPages;
+  final int rightPages;
+
+  @override
+  List<Object?> get props => [leftPages, rightPages];
+}
 
 class PagedGridCell extends Equatable {
   const PagedGridCell(this.pageIndex, this.col, this.row);
---
+++ test/unit_tests/pages_test.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:schildpad/home/pages.dart';
+
+void main() {
+  group('PagesStateNotifier tests', () {
+    test('A new PagesStateNotifier should have 0 left and 0 right pages', () {
+      final pagesStateNotifier = PagesStateNotifier();
+      expect(pagesStateNotifier.debugState.leftPages, 0);
+      expect(pagesStateNotifier.debugState.rightPages, 0);
+    });
+    test('Adding a left page should increase leftPages by 1', () {
+      final pagesStateNotifier = PagesStateNotifier();
+      final leftPagesBefore = pagesStateNotifier.debugState.leftPages;
+      pagesStateNotifier.addLeftPage();
+      expect(pagesStateNotifier.debugState.leftPages, leftPagesBefore + 1);
+    });
+    test('Adding a right page should increase rightPages by 1', () {
+      final pagesStateNotifier = PagesStateNotifier();
+      final rightPagesBefore = pagesStateNotifier.debugState.rightPages;
+      pagesStateNotifier.addRightPage();
+      expect(pagesStateNotifier.debugState.rightPages, rightPagesBefore + 1);
+    });
+
+    test('Removing a left page should decrease leftPages by 1', () {
+      final pagesStateNotifier = PagesStateNotifier();
+      pagesStateNotifier.addLeftPage();
+      final leftPagesBefore = pagesStateNotifier.debugState.leftPages;
+      pagesStateNotifier.removeLeftPage();
+      expect(pagesStateNotifier.debugState.leftPages, leftPagesBefore - 1);
+    });
+    test('Removing a right page should decrease rightPages by 1', () {
+      final pagesStateNotifier = PagesStateNotifier();
+      pagesStateNotifier.addRightPage();
+      final rightPagesBefore = pagesStateNotifier.debugState.rightPages;
+      pagesStateNotifier.removeRightPage();
+      expect(pagesStateNotifier.debugState.rightPages, rightPagesBefore - 1);
+    });
+
+    test('Removing a left page when leftPages is 0 should do nothing', () {
+      final pagesStateNotifier = PagesStateNotifier();
+      expect(pagesStateNotifier.debugState.leftPages, 0);
+      pagesStateNotifier.removeLeftPage();
+      expect(pagesStateNotifier.debugState.leftPages, 0);
+    });
+    test('Removing a right page when rightPages is 0 should do nothing', () {
+      final pagesStateNotifier = PagesStateNotifier();
+      expect(pagesStateNotifier.debugState.rightPages, 0);
+      pagesStateNotifier.removeRightPage();
+      expect(pagesStateNotifier.debugState.rightPages, 0);
+    });
+  });
+}
