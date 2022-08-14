d137d7be5d4d6c9258efce731d35680097ede9ee
Joel Koch <joel@joelkoch.dev>
2022-08-14 16:11:37 +0200

    enhance drag detector

--- lib/home/drag_detector.dart
+++ lib/home/drag_detector.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class DragDetector extends StatelessWidget {
+class DragDetector extends StatefulWidget {
   const DragDetector(
       {Key? key, required this.child, required this.onDragDetected})
       : super(key: key);
@@ -9,16 +9,39 @@ class DragDetector extends StatelessWidget {
   final VoidCallback onDragDetected;
 
   @override
+  State<DragDetector> createState() => _DragDetectorState();
+}
+
+class _DragDetectorState extends State<DragDetector> {
+  bool dragDetected = false;
+
+  @override
   Widget build(BuildContext context) {
     return Stack(children: [
-      child,
-      // DragTarget(
-      //     hitTestBehavior: HitTestBehavior.translucent,
-      //     onWillAccept: (_) {
-      //       onDragDetected();
-      //       return false;
-      //     },
-      //     builder: (_, __, ___) => const SizedBox.expand())
+      widget.child,
+      Positioned.fill(
+        child: DragTarget(
+            hitTestBehavior: HitTestBehavior.translucent,
+            onWillAccept: (_) {
+              if (!dragDetected) {
+                widget.onDragDetected();
+                setState(() {
+                  dragDetected = true;
+                });
+              }
+              return false;
+            },
+            onLeave: (_) {
+              if (dragDetected) {
+                setState(() {
+                  dragDetected = false;
+                });
+              }
+            },
+            builder: (_, __, ___) {
+              return const SizedBox.expand();
+            }),
+      ),
     ]);
   }
 }
