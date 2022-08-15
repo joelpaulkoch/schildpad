bdabf81fedadc02ec60bb17b5fef7fad4517348a
Joel Koch <joel@joelkoch.dev>
2022-08-15 21:39:20 +0200

    add AboutDialog to SettingsScreen

--- lib/settings/settings_screen.dart
+++ lib/settings/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:schildpad/theme/theme.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -7,6 +8,23 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+        systemOverlayStyle: schildpadSystemUiOverlayStyle,
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const Icon(Icons.info_outline_rounded),
+            title: const Text('About'),
+            onTap: () => showAboutDialog(
+              context: context,
+              applicationName: 'Schildpad',
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
