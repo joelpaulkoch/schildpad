89f9b34827bcb435b1fd70c38f15c9cd078ceca4
Joel Koch <joel@joelkoch.dev>
2022-08-15 22:17:42 +0200

    add reset button to SettingsScreen

--- lib/settings/settings_screen.dart
+++ lib/settings/settings_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:schildpad/home/dock.dart';
+import 'package:schildpad/home/home.dart';
+import 'package:schildpad/home/pages.dart';
 import 'package:schildpad/theme/theme.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -15,6 +19,7 @@ class SettingsScreen extends StatelessWidget {
       ),
       body: ListView(
         children: [
+          ResetListTile(),
           ListTile(
             leading: const Icon(Icons.info_outline_rounded),
             title: const Text('About'),
@@ -26,5 +31,33 @@ class SettingsScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class ResetListTile extends ConsumerWidget {
+  const ResetListTile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ListTile(
+        leading: const Icon(Icons.delete_forever_rounded),
+        title: const Text('Reset'),
+        onTap: () {
+          ref.read(dockGridStateProvider.notifier).removeAll();
+
+          final leftPages = ref.read(leftPagesProvider);
+          for (var i = -leftPages; i < 0; i++) {
+            ref.read(homeGridStateProvider(i).notifier).removeAll();
+            ref.read(pagesProvider.notifier).removeLeftPage();
+          }
+          final rightPages = ref.read(rightPagesProvider);
+          for (var i = rightPages; i > 0; i--) {
+            ref.read(homeGridStateProvider(i).notifier).removeAll();
+            ref.read(pagesProvider.notifier).removeRightPage();
+          }
+          ref.read(homeGridStateProvider(0).notifier).removeAll();
+        });
   }
 }
