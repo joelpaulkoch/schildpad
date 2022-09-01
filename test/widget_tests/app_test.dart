242a0c6b59779dfaa043971d2b189abf0b73d184
Joel Koch <joel@joelkoch.dev>
2022-09-01 07:53:40 +0000

    test if DartPluginRegistrant.ensureInitialized fixes path issue on macOS

--- test/widget_tests/app_test.dart
+++ test/widget_tests/app_test.dart
@@ -1,10 +1,12 @@
+import 'dart:ui';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:schildpad/main.dart';
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
 
   setUp(() async {
     await Hive.initFlutter('schildpad/app_test');
